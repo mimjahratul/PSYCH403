a349bf7fd0daeac8f883490c9b4e856b05fbba5d
mimjahratul <59889847+mimjahratul@users.noreply.github.com>
2022-09-26 12:01:39 -0600

    Add files via upload

---
+++ Assignment 2/yourname.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 26 10:44:08 2022
+
+@author: mimja
+"""
+
+print("J")
+print("A")
+print("H")
+print("R")
+print("A")
+print("T")
+print("U")
+print("L")
